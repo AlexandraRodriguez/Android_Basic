15d246bab91cddbb735e2dc07d886f400da05e8a
AlexandraRodriguez <ale.tunn9095@gmail.com>
2015-06-19 12:11:38 +0200

    First commit hello world project

---
+++ HelloWorld/app/src/main/java/com/alexandra/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.alexandra.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alexandra on 19/06/15.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.main_activity);
+    }
+}
